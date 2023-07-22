5499f5d7136a3e555b69a0e6cd966d767ce3f66d
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-07-22 11:11:19 +0100

    criando os itens de coleta e fazendo o javascript

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -23,11 +23,14 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
 
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+  citySelect.disabled = true
+
   fetch(url)
     .then((res) => res.json())
     .then((cities) => {
       for (const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       }
       citySelect.disabled = false
     })
@@ -37,3 +40,30 @@ document.querySelector("select[name=uf]").addEventListener("change", getCities)
 
 // * Itens de coleta Ends
 
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) { 
+  item.addEventListener("click", handleSelectedItem)
+} 
+
+let selectedItems = []
+
+function handleSelectedItem(event) { 
+  const itemLi = event.target
+
+  itemLi.classList.toggle("selected")
+
+  const itemId = itemLi.dataset.id
+
+  const alreadySelected = selectedItems.findIndex(item => item === itemId)
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter(item => item != itemId)
+    selectedItems = filteredItems
+  } else {
+    selectedItems.push(itemId)
+  }
+
+  collectedItems.value = selectedItems
+}
